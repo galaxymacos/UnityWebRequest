3fb5ad78cca53e40a07f8f4138c3b8d2f1126aea
Xun Ruan <galaxymacos@gmail.com>
2020-06-16 14:25:24 -0400

    Pass Json Object From JS to Unity; Send Message from JS to Unity

---
+++ Assets/JavascriptHook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JavascriptHook : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer circleSpriteRenderer;
+    [SerializeField] private Text stringText;
+    [SerializeField] private Text numText;
+
+    public void TintRed()
+    {
+        circleSpriteRenderer.color = Color.red;
+    }
+
+    public void TintGreen()
+    {
+        circleSpriteRenderer.color = Color.green;
+    }
+
+    public void TestJson(string json)
+    {
+        JsonObject jsonObject = JsonUtility.FromJson<JsonObject>(json);
+        stringText.text = $"Name: {jsonObject.name}; Age: {jsonObject.age}";
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TintRed();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            TintGreen();
+        }
+    }
+
+    public class JsonObject
+    {
+        public string name;
+        public int age;
+    }
+}
